d090eb4216c7977939a125340e1e2e6e4cdba06d
Dan Webb <d.c.webb1993@gmail.com>
2018-03-18 16:20:28 +0700

    Add font prop type

--- src/types.js
+++ src/types.js
@@ -1,6 +1,11 @@
 import {oneOf} from 'prop-types';
 import color from './theme/color';
+import font from './theme/font';
 
 const colorType = oneOf(Object.keys(color));
+const fontType = oneOf(font.weights);
 
-export {colorType};
+export {
+	colorType,
+	fontType
+};
